af49b66471b8d28c20cdca78cc3e4e6079fc53e1
Anant Kaushik <anant.kaushik2@gmail.com>
2018-07-05 07:03:29 +0530

    Polynomial Regression

---
+++ Regression/Polynomial-Regression/polynomial_regression.py
@@ -0,0 +1,43 @@
+# Polynomial Regression
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Making Test Set
+x_test = [[1.5],[2.5],[3.9],[4.5],[5.5],[6.8],[7.7],[8.3],[9.2],[9.9]]
+
+# Visualising the Polynomial Regression results
+"""plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()"""
+
+
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.scatter(x_test, lin_reg_2.predict(poly_reg.fit_transform(x_test)), color = 'green')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Polynomial Regression')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
